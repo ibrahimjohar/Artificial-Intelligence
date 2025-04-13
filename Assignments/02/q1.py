e11a2be3e78ddf3bd204d6c525c0f76a9733a0d3
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-04-13 23:41:07 +0500

    Update q1.py

--- Assignments/02/q1.py
+++ Assignments/02/q1.py
@@ -1,1 +1,15 @@
 #q1
+
+def query(x: int) -> int:
+    return -1 * (x-7)**2 + 49
+
+def find_peak(N: int) -> int:
+    curr = 0
+    while curr < N:
+        if query(curr) < query(curr+1):
+            curr += 1
+        else:
+            break
+    return curr
+
+print(find_peak(60))
